4061262c382ff2d901a9680cf055a264fbb227d8
Rakshith Ravi <rakshith.ravi@gmx.com>
2025-05-12 06:40:55 +0000

    Refactor prelude and app_route module imports for clarity and consistency

--- frontend/common/src/utils/app_route.rs
+++ frontend/common/src/utils/app_route.rs
@@ -1,25 +1,40 @@
 use std::marker::PhantomData;
 
 use axum_extra::routing::TypedPath;
-use leptos::*;
+use leptos::either::Either;
 use leptos_router::{
-	Params,
-	Route,
-	use_params as use_router_params,
-	use_query as use_router_query,
+	components::Route,
+	hooks::{
+		use_location,
+		use_navigate,
+		use_params as use_router_params,
+		use_query as use_router_query,
+	},
+	params::Params,
+	path,
 };
 use serde::{Serialize, de::DeserializeOwned};
+
+use crate::prelude::*;
 
 /// A trait for types that can be used as a route in the application.
 /// It also provides the path as well as the query parameters for the route.
 pub trait TypedRoute:
-	TypedPath + Params + DeserializeOwned + Serialize + PartialEq + Clone + 'static
+	TypedPath + Params + DeserializeOwned + Serialize + PartialEq + Clone + Send + Sync + 'static
 {
 	/// Whether the route requires the user to be logged in.
 	const REQUIRES_LOGIN: bool;
 
 	/// The query parameters for the route.
-	type Query: Params + DeserializeOwned + Serialize + PartialEq + Clone + Default + 'static;
+	type Query: Params
+		+ DeserializeOwned
+		+ Serialize
+		+ PartialEq
+		+ Clone
+		+ Default
+		+ Send
+		+ Sync
+		+ 'static;
 }
 
 #[component(transparent)]
@@ -29,23 +44,41 @@ pub fn AppRoute<R, F, V>(
 	_phantom: PhantomData<R>,
 	/// The view for the route
 	view: F,
-	/// The Children of the route
-	#[prop(optional, default = Box::new(|| Fragment::new(vec![])))]
-	children: Children,
 ) -> impl IntoView
 where
 	R: TypedRoute,
-	F: Fn(R::Query, R) -> V + 'static,
-	V: IntoView,
+	F: Fn(R::Query, R) -> V + Send + Sync + 'static,
+	V: IntoView + 'static,
 {
 	let query: R::Query = use_router_query().get_untracked().unwrap_or_default();
 	let params: R = use_router_params()
 		.get_untracked()
 		.expect("cannot parse params");
+	let auth_state = expect_context::<AuthState>();
+	let navigate = use_navigate();
+	let location = use_location();
 
-	view! {
-		<Route view={move || view(query.clone(), params.clone())} path={<R as TypedPath>::PATH}>
-			{children()}
-		</Route>
+	Effect::new(move || {
+		let params = use_router_query::<R::Query>().get();
+		if params.is_err() {
+			navigate(
+				&format!(
+					"{}{}",
+					location.pathname.get(),
+					serde_urlencoded::to_string(&R::Query::default()).unwrap()
+				),
+				Default::default(),
+			);
+		}
+	});
+
+	if R::REQUIRES_LOGIN {
+		Either::Left(())
+	} else {
+		Either::Right(view! {
+			<Route
+				path=path!("users")
+				view={move || view(query.clone(), params.clone())} />
+		})
 	}
 }
